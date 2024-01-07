31e2541473b391b7cb94639d7a6469d4a346752e
Ganeshdarshan Venkatraman Bhat <88202395+bhatganeshdarshan@users.noreply.github.com>
2024-01-07 15:12:50 +0530

    crop prediction website working

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 import pickle
-import sklearn
 
-prediction_model = pickle.load(open("prediction_model.pkl","rb"))
+prediction_model = pickle.load(open("prediction_model.pkl", "rb"))
+df = pd.read_csv("crop_npk_dataset_final.csv")
+df = df.drop('label', axis=1)
 
 app = Flask(__name__)
 
@@ -12,36 +13,36 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/crop_prediction',methods=['POST'])
-def prediction():
-    n=request.form['Nitrogen']
-    p=request.form['Phosphorous']
-    k=request.form['Potassium']
-    temp=request.form['Temperature']
-    hum=request.form['Humidity']
-    pH = request.form['pH']
-    rf = request.form['Rainfall']
-    featured_data = {'columns':list('N','P','K','temperature','humidity','ph','rainfall')}
-    test_series = pd.Series(np.zeros(7),index=featured_data['columns'])
-    test_series['N']=n
-    test_series['P']=p
-    test_series['K']=k
-    test_series['temperature'] =temp
-    test_series['humidity']=hum
-    test_series['ph']=pH
-    test_series['rainfall'] =rf
+
+@app.route("/crop_prediction",methods=['POST'])
+def predict():
+    N = request.form['Nitrogen']
+    P = request.form['Phosphorous']
+    K = request.form['Potassium']
+    temp = request.form['Temperature']
+    humidity = request.form['Humidity']
+    ph = request.form['pH']
+    rainfall = request.form['Rainfall']
+
+    feature_list = [N, P, K, temp, humidity, ph, rainfall]
+    single_pred = np.array(feature_list).reshape(1, -1)
+    prediction = prediction_model.predict(single_pred)
+
     crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
                  8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
                  14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
                  19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
-    predict_crop = prediction_model.predict(test_series)
-    if predict_crop in crop_dict:
-        crop = crop_dict[predict_crop]
-        res = f"{crop} is the best crop to cultivate"
+
+    if prediction[0] in crop_dict:
+        crop = crop_dict[prediction[0]]
+        result = f"{crop} is the best crop to cultivate"
     else:
-        res = "No suitable crop found for the given data !"
-    return render_template('index.html',result=res)
+        result = "Could not find any suitable crops for this data"
+    return render_template('index.html',result = result)
 
-if __name__=="__main__":
+
+
+
+# python main
+if __name__ == "__main__":
     app.run(debug=True)
-
